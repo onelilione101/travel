b0363c2e45fdd50a2328caad32681d802ef3aa37
lili <376578677@qq.com>
2019-06-10 11:30:01 +0800

    home-swiper

--- travel/src/main.js
+++ travel/src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'  //这里直接写styles是因为已经在webpack的配置项中 设置了简写
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)  //attach是fastclick自带的方法
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
+    <!-- <swiper-slide><img class="swiper-img" src="~img/desert.png"></swiper-slide>    //!!!!!直接在标签里写src可以用我设的缩写 ~img，放到data里就行不通，会报错
+    <swiper-slide><img class="swiper-img" src="~img/mountain.png"></swiper-slide>
+    <swiper-slide><img class="swiper-img" src="~img/galaxy.png"></swiper-slide> -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+    name:'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination:'.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl:require('../../../assets/img/desert.png')      //!!!!!   本地图片加个require，然后把img地址包起来就可以了
+            },{
+                id: '0002',
+                imgUrl:require('../../../assets/img/mountain.png')   
+            },{
+                id: '0003',
+                imgUrl:require('../../../assets/img/galaxy.png')    
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: rgba(255,255,255,0.7)
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.2%     //根据使用图片的高/宽*100 形成百分比  这里我直接用的是375*117 所以比例是31.2% 如果原图尺寸不是375*117 那么按原图的
+    background: #eee    // eee比ccc更淡
+    .swiper-img
+      width: 100%
+    
+</style>
+
+
