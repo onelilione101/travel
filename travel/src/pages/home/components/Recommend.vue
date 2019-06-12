d12cfb668d21d799c57259a02f51330a0b1e8088
lili <376578677@qq.com>
2019-06-12 23:44:58 +0800

    add icons

---
+++ travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="wrapper">热销推荐</div>
+</template>
+
+<script>
+export default {
+    name:'HomeRecommend',
+    data () {
+        return {
+            
+    }
+ }
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+
+</style>
