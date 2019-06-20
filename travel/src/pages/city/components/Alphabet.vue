9db9ca8bb554aea22d3b9a621e26d0f2547817e4
lili <376578677@qq.com>
2019-06-20 20:35:24 +0800

    perfomance

--- travel/src/pages/city/components/Alphabet.vue
+++ travel/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
  <ul class="list">
-   <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+   <li class="item" 
+    v-for="item of letters"
+    :key="item"
+    :ref="item"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    @click="handleLetterClick"
+    >{{item}}
+    </li>
   </ul>
 </template>
 
@@ -10,11 +19,52 @@ export default {
     props: {
         cities: Object
     },
+    computed: {
+      letters () {
+        const letters = []
+        for(let i in this.cities) {
+          letters.push(i)
+        }
+        return letters
+      }
+    },
     data () {
         return {
+          touchStatus: false,
+          startY: 0,
+          timer: null
+      }
+    },
+    updated () {
+      this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods: {
+      handleLetterClick (e) {
+        this.$emit('change', e.target.innerText)
+      },
+      handleTouchStart () {
+        this.touchStatus = true
+      },
+      handleTouchMove (e) {
+        if(this.touchStatus){
 
+            if (this.timer) {
+                clearTimeout(this.timer)
+           }
+            this.timer = setTimeout(() => {
+               const touchY = e.touches[0].clientY - 79
+               const index = Math.floor((touchY - this.startY) / 20)
+               if (index >= 0 && index < this.letters.length){
+               this.$emit('change',this.letters[index])
+              }
+            },16) 
+        }
+      },
+      handleTouchEnd () {
+        this.touchStatus = false
+      }
     }
- }
+    
 }
 </script>
 
@@ -29,7 +79,6 @@ export default {
        right: 0
        bottom: 0
        width: .5rem
-       padding-top: 1.3rem
        text-align: center 
        .item
          line-height: .4rem
